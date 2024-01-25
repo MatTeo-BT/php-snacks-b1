523dd7f7b350261fa0d6c01bf9d506c94a686747
MatTeo-BT <aboutmet@gmail.com>
2024-01-25 12:43:54 +0100

    create proprietà e costruttore

---
+++ snack-3-OOP/index.php
@@ -0,0 +1,24 @@
+<!-- Create una nuova classe che rappresenti un Pokemon e che abbia almeno cinque proprieta, tre metodi ed il costruttore.
+Microbonus: creo un'altra classe che aggiungo al pokemon per composizione che configuri il tipo di pokemon (aria, terra, ecc.) -->
+
+<?php
+class Pokemon
+{
+
+    private $nome;
+    private $tipo;
+    private $livello;
+    private $evoluzione;
+    private $mosse;
+
+    public function __construct($_nome, $_tipo, $_livello, $_evoluzione, $_mosse)
+    {
+        $this->nome = $_nome;
+        $this->tipo = $_tipo;
+        $this->livello = $_livello;
+        $this->evoluzione = $_evoluzione;
+        $this->mosse = $_mosse;
+    }
+}
+
+?>
